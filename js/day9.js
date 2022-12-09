5c5d31665e60428dd4801777e4bce8688d6ab791
Annika Paulheim <annika.paulheim@technia.com>
2022-12-09 10:20:13 +0100

    day9 js

--- js/day9.js
+++ js/day9.js
@@ -22,10 +22,9 @@ const moveTail = (h, t) => {
     if (sub[0]) t[0] = t[0] + sub[0];
     t[1] = t[1] + sub[1];
   }
-  tailPositions.add(`${t[0]},${t[1]}`);
 };
 
-const moveHeadOne = (h, t, dir) => {
+const moveHeadOne = (h, dir) => {
   switch (dir) {
     case "U":
       h[1] = h[1] + 1;
@@ -39,7 +38,6 @@ const moveHeadOne = (h, t, dir) => {
     case "L":
       h[0] = h[0] - 1;
   }
-  moveTail(h, t);
 };
 
 const draw = (h, t) => {
@@ -61,7 +59,9 @@ for (let i = 0; i < directions.length; i++) {
   const [dir, numStr] = directions[i].split(" ");
   const num = parseInt(numStr);
   for (let j = 0; j < num; j++) {
-    moveHeadOne(head, tail, dir);
+    moveHeadOne(head, dir);
+    moveTail(head, tail);
+    tailPositions.add(`${tail[0]},${tail[1]}`);
     // draw(head, tail);
   }
 }
@@ -82,38 +82,14 @@ let rope = [
 
 const tailPositionsGold = new Set();
 
-const moveTailGold = (h, t) => {
-  if (Math.abs(h[0] - t[0]) > 1 || Math.abs(h[1] - t[1]) > 1) {
-    const sub = [floatToInt((h[0] - t[0]) / 2), floatToInt((h[1] - t[1]) / 2)];
-    if (sub[0]) t[0] = t[0] + sub[0];
-    t[1] = t[1] + sub[1];
-  }
-};
-
-const moveHeadOneGold = (h, dir) => {
-  switch (dir) {
-    case "U":
-      h[1] = h[1] + 1;
-      break;
-    case "R":
-      h[0] = h[0] + 1;
-      break;
-    case "D":
-      h[1] = h[1] - 1;
-      break;
-    case "L":
-      h[0] = h[0] - 1;
-  }
-};
-
 // gold
 for (let i = 0; i < directions.length; i++) {
   const [dir, numStr] = directions[i].split(" ");
   const num = parseInt(numStr);
   for (let j = 0; j < num; j++) {
-    moveHeadOneGold(rope[0], dir);
+    moveHeadOne(rope[0], dir);
     for (let k = 1; k < rope.length; k++) {
-      moveTailGold(rope[k - 1], rope[k]);
+      moveTail(rope[k - 1], rope[k]);
     }
     tailPositionsGold.add(`${rope[9][0]},${rope[9][1]}`);
   }
