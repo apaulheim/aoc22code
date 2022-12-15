03dada2f65b2d0ca4397eca3391f86a5e18e9da1
Annika Paulheim <annika.paulheim@technia.com>
2022-12-15 15:33:35 +0100

    day15 js pretty

--- js/day15.js
+++ js/day15.js
@@ -9,103 +9,106 @@ let data = input
       .split(", ")
       .map((val) => parseInt(val.substring(2))),
     line[1].split(", ").map((val) => parseInt(val.substring(2))),
-  ]);
+  ])
+  .map((pair) => ({
+    sensor: { x: pair[0][0], y: pair[0][1] },
+    beacon: { x: pair[1][0], y: pair[1][1] },
+  }));
 
 const manhattan = (sensor, beacon) => {
-  return Math.abs(sensor[0] - beacon[0]) + Math.abs(sensor[1] - beacon[1]);
+  return Math.abs(sensor.x - beacon.x) + Math.abs(sensor.y - beacon.y);
 };
 
 const getRangeInLine = (sensor, beacon, line) => {
   const m = manhattan(sensor, beacon);
-  const distance = Math.abs(line - sensor[1]);
+  const distance = Math.abs(line - sensor.y);
   // sensor is too far away
   if (distance > m) return null;
   else {
-    return [sensor[0] - (m - distance), sensor[0] + (m - distance)];
+    return { min: sensor.x - (m - distance), max: sensor.x + (m - distance) };
   }
 };
 
 const isInRange = (value, range) => {
-  return value >= range[0] && value <= range[1];
+  return value >= range.min && value <= range.max;
 };
 
 const silver = (lineNr) => {
   const ranges = data
-    .map((pair) => getRangeInLine(pair[0], pair[1], lineNr))
+    .map((pair) => getRangeInLine(pair.sensor, pair.beacon, lineNr))
     .filter((range) => range != null);
 
   let beaconsInLine = 0;
   const beaconsSet = new Set();
   data.forEach((pair) => {
-    if (pair[1][1] == lineNr) {
-      if (!beaconsSet.has(JSON.stringify(pair[1]))) {
+    if (pair.beacon.y == lineNr) {
+      if (!beaconsSet.has(JSON.stringify(pair.beacon))) {
         beaconsInLine++;
-        beaconsSet.add(JSON.stringify(pair[1]));
+        beaconsSet.add(JSON.stringify(pair.beacon));
       }
     }
   });
 
   const minX = ranges.reduce(
-    (acc, val) => Math.min(acc, val[0]),
+    (acc, val) => Math.min(acc, val.min),
     Number.POSITIVE_INFINITY
   );
   const maxX = ranges.reduce(
-    (acc, val) => Math.max(acc, val[1]),
+    (acc, val) => Math.max(acc, val.max),
     Number.NEGATIVE_INFINITY
   );
 
   let silver = 0;
-  let l = "";
   for (let i = minX; i <= maxX; i++) {
     silver += ranges.some((range) => isInRange(i, range)) ? 1 : 0;
-    l += ranges.some((range) => isInRange(i, range)) ? "#" : ".";
   }
   return silver - beaconsInLine;
 };
 console.log("silver:", silver(2000000));
 
+// GOLD
 const beaconsSet = new Set();
 data.forEach((pair) => {
-  beaconsSet.add(JSON.stringify(pair[1]));
+  beaconsSet.add(JSON.stringify(pair.beacon));
 });
 
 const isOverlapping = (range1, range2) => {
   // range 1 and 2 are sorted
-  return range2[0] <= range1[1];
+  return range2.min <= range1.max;
 };
 
-const findUnicorn = (ranges, y, lineWidth) => {
+const findSpotInLine = (ranges, y, lineWidth) => {
   let i = 1;
-  let min = ranges[i - 1][0];
-  let max = ranges[i - 1][1];
+  let min = ranges[i - 1].min;
+  let max = ranges[i - 1].max;
   while (i < ranges.length && max <= lineWidth) {
-    let over = isOverlapping([min, max], ranges[i]);
+    let over = isOverlapping({ min, max }, ranges[i]);
     if (over) {
-      max = Math.max(max, ranges[i][1]);
+      max = Math.max(max, ranges[i].max);
       i++;
     } else {
       let v = max + 1;
-      while (v < ranges[i][0]) {
-        if (!beaconsSet.has(JSON.stringify([v, y]))) return [v, y];
+      while (v < ranges[i].min) {
+        if (!beaconsSet.has(JSON.stringify({ x: v, y }))) return [v, y];
         v++;
       }
-      max = ranges[i][0];
+      max = ranges[i].min;
     }
   }
   return false;
 };
 
-const silverGold = (lineNr) => {
+const checkLine = (lineNr) => {
   const ranges = data
-    .map((pair) => getRangeInLine(pair[0], pair[1], lineNr))
+    .map((pair) => getRangeInLine(pair.sensor, pair.beacon, lineNr))
     .filter((range) => range != null)
-    .sort((a, b) => a[0] - b[0]);
-  return findUnicorn(ranges, lineNr, 4000000);
+    .sort((a, b) => a.min - b.min);
+  return findSpotInLine(ranges, lineNr, 4000000);
 };
 
 const gold = () => {
   for (let i = 0; i < 4000000; i++) {
-    let res = silverGold(i);
+    let res = checkLine(i);
     if (res) return res[0] * 4000000 + res[1];
   }
   return "sad";
